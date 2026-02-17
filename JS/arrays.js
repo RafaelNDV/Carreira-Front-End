576806dfef0c637bbe8c44ee4144509b6a3c4926
Rafael <rafaelnestor01@gmail.com>
2026-02-17 01:12:20 -0300

    Começando a aprender sobre arrays

---
+++ JS/arrays.js
@@ -0,0 +1,14 @@
+const frutas = ['Uva', 'Banana', 'Kiwi', 'Maçã', 'Morango']
+
+console.log(frutas)
+
+console.log('Primeira fruta', frutas[0])
+console.log('Última fruta: ', frutas[frutas.length - 1])
+
+console.log('Total de frutas: ', frutas.length)
+
+frutas.push('Melancia')
+
+console.log('Total de frutas depois de adicionar melância: ', frutas.length)
+
+console.log('Última fruta: ', frutas[frutas.length - 1])
