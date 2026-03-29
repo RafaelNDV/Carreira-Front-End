0e55d6bd6225476faabc83f169195953e1897c86
Rafael <rafaelnestor01@gmail.com>
2026-03-29 13:24:20 -0300

    Aprendendo sobre componentes escaláveis

--- to-do-list/src/components/TodoForm/index.jsx
+++ to-do-list/src/components/TodoForm/index.jsx
@@ -5,7 +5,7 @@ import { SaveButton } from '../SaveButton'
 export function TodoForm ({ onSubmit }){
     return(
         <form action={ onSubmit } className="form-salvar-item">
-            <TextInput required placeholder='Digite o item que deseja adicionar'/>
+            <TextInput required placeholder='Digite o item que deseja adicionar' name='description'/>
             <SaveButton>Salvar item</SaveButton>
         </form>
     )
