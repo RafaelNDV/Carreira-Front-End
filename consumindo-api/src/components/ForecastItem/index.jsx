e4dff5dedbae2ec60177355e33b36789b9e84f55
Rafael <rafaelnestor01@gmail.com>
2026-04-21 21:45:07 -0300

    fazendo apenas uma atividade

--- consumindo-api/src/components/ForecastItem/index.jsx
+++ consumindo-api/src/components/ForecastItem/index.jsx
@@ -1,9 +1,39 @@
 import styles from './forecast-item.module.css'
+import { useState } from 'react'
 
 export const ForecastItem = ({ date, min, max, condition, description, weekday }) => {
+
+    let dayWeek = ''
+
+    const markWeekDay = () => {
+        if (weekday == 'Seg'){
+            dayWeek = 'Segunda'
+        }
+        else if (weekday == 'Ter'){
+            dayWeek = 'Terça'
+        }
+        else if(weekday == 'Qua'){
+            dayWeek = 'Quarta'
+        }
+        else if(weekday == 'Qui'){
+            dayWeek = 'Quinta'
+        }
+        else if(weekday == 'Sex'){
+           dayWeek = 'Sexta'
+        }
+        else if(weekday == 'Sáb'){
+            dayWeek = 'Sábado'
+        }
+        else if(weekday == 'Dom'){
+            dayWeek = 'Domingo'
+        }
+    }
+
+    markWeekDay()
+
     return (
         <div className={styles.container}>
-            <h3 className={styles.weekday}> {weekday} </h3>
+            <h3 className={styles.weekday}> {dayWeek} </h3>
             <p> ({date}) </p>
             <img src={`./icons-weather/${condition}.svg`} alt={description} />
             <p> {min}/{max}º</p>
