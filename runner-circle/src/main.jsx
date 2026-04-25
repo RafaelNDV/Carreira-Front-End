85d0777e694d5016996dece1c76c291c497f96a8
Rafael <rafaelnestor01@gmail.com>
2026-04-25 13:40:04 -0300

    fazendo as queries do graphql no codigo, e aprendendo o hook use query, e outras coisas

--- runner-circle/src/main.jsx
+++ runner-circle/src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ApolloClient, InMemoryCache} from '@apollo/client'
+import { ApolloProvider } from "@apollo/client/react";
+import { HttpLink } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "http://localhost:3001/graphql",
+  }),
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
 )
